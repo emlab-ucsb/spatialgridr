46bb76c089ac4853f1af8f0e1de27767f31ee04b
Jason Flower <jflowernet@aim.com>
2024-03-06 16:46:42 +1000

    testing sf categorical to sf grid

--- R/sf_to_grid_cov_fraction_test.R
+++ R/sf_to_grid_cov_fraction_test.R
@@ -29,9 +29,45 @@ plot(sf_to_raster_by_coverage_fraction)
 
 grouping_col <- "POP"
 
-sf_to_raster_cont_values <- rasterize(lux_sf, r,  field = grouping_col, fun = "mean")
+sf_to_raster_cont_values <- rasterize(vect(lux_sf), r,  field = grouping_col, fun = 'mean', cover=T)
 
-plot(sf_to_raster_cont_values)
+plot(sf_to_raster_cont_values, type = "classes")
+lines(vect(lux_sf))
 
-#using a coverage approach
-exactextractr::coverage_fraction(r, lux_sf |> dplyr::select("POP"))
+#using a coverage approach -would be possible, but would need to create a raster layer for each value in the sf object, so would potentially be memory heavy
+exactextractr::coverage_fraction(r, lux_sf |> dplyr::select("POP")) |> rast() |> plot()
+
+#sf to sf grid
+
+#sf categorical to grid
+sf_grid <- sf::st_as_sf(as.polygons(r, dissolve = FALSE))
+
+#current method
+presence_absence <- lux_sf %>%
+  sf::st_intersects(sf_grid, .) %>%
+  {lengths(.)>0} %>%
+  as.integer()
+
+sf_grid %>%
+  dplyr::mutate(presence := presence_absence, .before = 1) %>%
+  plot()
+
+#new approach
+grid_data_intersection <- sf_grid %>%
+  dplyr::mutate(cellID = 1:nrow(.)) %>%
+  sf::st_intersection(lux_sf %>% select("NAME_2"))
+
+plot(test)
+
+shared_area <- grid_data_intersection %>%
+  mutate(shared_area = sf::st_area(sf::st_geometry(.)) %>% as.numeric(), .before = 1)
+
+grid_cell_areas <- sf_grid %>%
+  dplyr::mutate(cellID = 1:nrow(.)) %>%
+  mutate(area_cell = sf::st_area(sf::st_geometry(.)) %>% as.numeric(), .before = 1)
+
+grid_data_coverage <- grid_cell_areas %>%
+  dplyr::full_join(shared_area %>% sf::st_drop_geometry(), by = dplyr::join_by(cellID)) %>%
+  mutate(area_overlap = shared_area/area_cell, .keep = "unused")
+
+plot(grid_data_coverage["area_overlap"])
