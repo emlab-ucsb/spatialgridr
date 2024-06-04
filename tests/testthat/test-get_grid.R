2c7c5a86645c51871a35647dc4288eaa8b51118f
Jason Flower <jflowernet@aim.com>
2024-06-04 14:47:04 +1000

    overhaul of get_grid arguments, adding touches = argument for raster as well as sf

--- tests/testthat/test-get_grid.R
+++ tests/testthat/test-get_grid.R
@@ -1,29 +1,29 @@
 test_that("return grid for Samoa - raster", {
   expect_s4_class(get_grid(boundary = get_boundary(name = "Samoa", type = "eez", country_type = "country"),
-                                    projection_crs = '+proj=laea +lon_0=-172.5 +lat_0=0 +datum=WGS84 +units=m +no_defs',
+                                    crs = '+proj=laea +lon_0=-172.5 +lat_0=0 +datum=WGS84 +units=m +no_defs',
                                     resolution = 5000),
                   class = "SpatRaster")
 })
 
 test_that("return grid for Samoa - sf square", {
   expect_s3_class(get_grid(boundary = get_boundary(name = "Samoa", type = "eez", country_type = "country"),
-                                    projection_crs = '+proj=laea +lon_0=-172.5 +lat_0=0 +datum=WGS84 +units=m +no_defs',
-                                    option = "sf_square",
+                                    crs = '+proj=laea +lon_0=-172.5 +lat_0=0 +datum=WGS84 +units=m +no_defs',
+                                    output = "sf_square",
                                     resolution = 5000),
                   class = "sf")
 })
 
 test_that("return grid for Samoa - sf hex", {
   expect_s3_class(get_grid(boundary = get_boundary(name = "Samoa", type = "eez", country_type = "country"),
-                                    projection_crs = '+proj=laea +lon_0=-172.5 +lat_0=0 +datum=WGS84 +units=m +no_defs',
-                                    option = "sf_hex",
+                                    crs = '+proj=laea +lon_0=-172.5 +lat_0=0 +datum=WGS84 +units=m +no_defs',
+                                    output = "sf_hex",
                                     resolution = 5000),
                   class = "sf")
 })
 
 test_that("return grid for kiribati - raster", {
   expect_s4_class(get_grid(boundary =  get_boundary(name = "Kiribati", type = "eez", country_type = "sovereign"),
-                                    projection_crs = '+proj=laea +lon_0=-159.609375 +lat_0=0 +datum=WGS84 +units=m +no_defs',
+                                    crs = '+proj=laea +lon_0=-159.609375 +lat_0=0 +datum=WGS84 +units=m +no_defs',
                                     resolution = 5000),
                   class = "SpatRaster")
 })
@@ -32,8 +32,8 @@ test_that("return grid for kiribati - raster", {
 
 test_that("return planning grid for kiribati - sf square", {
   expect_s3_class(get_grid(boundary =  get_boundary(name = "Kiribati", type = "eez", country_type = "sovereign"),
-                                    projection_crs = '+proj=laea +lon_0=-159.609375 +lat_0=0 +datum=WGS84 +units=m +no_defs',
-                                    option = "sf_square",
+                                    crs = '+proj=laea +lon_0=-159.609375 +lat_0=0 +datum=WGS84 +units=m +no_defs',
+                                    output = "sf_square",
                                     resolution = 5000),
                   class = "sf")
 })
@@ -42,8 +42,8 @@ test_that("return planning grid for kiribati - sf square", {
 
 test_that("return planning grid for kiribati - raster", {
   expect_s3_class(get_grid(boundary =  get_boundary(name = "Kiribati", type = "eez", country_type = "sovereign"),
-                                    projection_crs = '+proj=laea +lon_0=-159.609375 +lat_0=0 +datum=WGS84 +units=m +no_defs',
-                                    option = "sf_hex",
+                                    crs = '+proj=laea +lon_0=-159.609375 +lat_0=0 +datum=WGS84 +units=m +no_defs',
+                                    output = "sf_hex",
                                     resolution = 5000),
                   class = "sf")
 })
