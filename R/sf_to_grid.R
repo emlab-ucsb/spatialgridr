b63913f4cd844a6bbdeb4a72c72ee0465190ceda
Jason Flower <jflowernet@aim.com>
2026-05-05 18:49:40 +1000

    remove more pipes

--- R/sf_to_grid.R
+++ R/sf_to_grid.R
@@ -26,16 +26,25 @@ sf_to_grid <- function(spatial_grid, dat, matching_crs, name, feature_names, ant
       sf::st_transform(sf::st_crs(dat)) |>
       (\(x) if(antimeridian) sf::st_shift_longitude(x) else x)()
 
-    #START FINAL CODE REFACTORING HERE
-    dat_cropped <- dat
-      {if(all(antimeridian & !(c("POINT", "MULTIPOINT") %in% unique(sf::st_geometry_type(.))))) {
-        sf::st_break_antimeridian(., lon_0 = 180) %>% sf::st_shift_longitude()}
-        else if(all(antimeridian & (c("POINT", "MULTIPOINT") %in% unique(sf::st_geometry_type(.))))){
-          sf::st_shift_longitude(.)} else .} %>%
-      sf::st_crop(grid_temp) %>%
-      sf::st_transform(sf::st_crs(spatial_grid)) %>%
-      {if(all(sf::st_is_valid(.))) . else sf::st_make_valid(.)} %>%
-      {if(any(sf::st_is(., c("POINT", "MULTIPOINT")), all(sf::st_is(., "POLYGON")),  all(sf::st_is(., "MULTIPOLYGON")))) . else sf::st_collection_extract(., "POLYGON")} #after cropping, can end up with mixed geometry type: get only polygons (includes MULTIPOLYGON)
+    if(antimeridian){
+      if(!(c("POINT", "MULTIPOINT") %in% unique(sf::st_geometry_type(.)))){
+        dat_temp <- dat |>
+          sf::st_break_antimeridian(lon_0 = 180) |>
+          sf::st_shift_longitude()
+      } else{
+        data_temp <- sf::st_shift_longitude(dat)
+      }
+    } else{
+      dat_temp <- dat
+    }
+
+    dat_cropped <- dat_temp |>
+      sf::st_crop(grid_temp) |>
+      sf::st_transform(sf::st_crs(spatial_grid)) |>
+      (\(x) if (all(sf::st_is_valid(x))) x else sf::st_make_valid(x))() |>
+      #after cropping, can end up with mixed geometry type: get only polygons (includes MULTIPOLYGON)
+      (\(x) if(any(sf::st_is(x, c("POINT", "MULTIPOINT")), all(sf::st_is(x, "POLYGON")),  all(sf::st_is(x, "MULTIPOLYGON")))) x else sf::st_collection_extract(x, "POLYGON"))()
+
   }
 
   if(is.null(feature_names)){
@@ -49,17 +58,16 @@ sf_to_grid <- function(spatial_grid, dat, matching_crs, name, feature_names, ant
                  terra::subst(1,0) |>
                  setNames(name))
       } else{
-        return(spatial_grid %>%
-                 dplyr::mutate({{name}} := 0, .before = ncol(.)))
+        return(dplyr::mutate(spatial_grid, {{name}} := 0, .before = ncol(spatial_grid)))
       }
     }
 
-    dat_grouped <- dat_cropped %>%
-      dplyr::mutate({{name}} := 1, .before = 1) %>%
-      dplyr::group_by({{name}}) %>%
-      dplyr::summarise() %>%
-      dplyr::ungroup() %>%
-      {if(all(c("POLYGON", "MULTIPOLYGON") %in% (sf::st_geometry_type(.) %>% unique() %>% as.character()))) sf::st_cast(., to = "MULTIPOLYGON") else .}
+    dat_grouped <- dat_cropped |>
+      dplyr::mutate({{name}} := 1, .before = 1) |>
+      dplyr::group_by({{name}}) |>
+      dplyr::summarise() |>
+      dplyr::ungroup() |>
+      (\(x) if(all(c("POLYGON", "MULTIPOLYGON") %in% (sf::st_geometry_type(x) |>  unique() |>  as.character()))) sf::st_cast(x, to = "MULTIPOLYGON") else x)()
 
   }  else {
     #return raster or sf object with zeroes if no data in the spatial grid
@@ -69,32 +77,32 @@ sf_to_grid <- function(spatial_grid, dat, matching_crs, name, feature_names, ant
       layer_names <- unique(dat[[feature_names]])
 
       if(is_raster){
-        return(spatial_grid %>%
-                 terra::subst(1,0) %>%
-                 rep(length(layer_names)) %>%
+        return(spatial_grid |>
+                 terra::subst(1,0) |>
+                 rep(length(layer_names)) |>
                  setNames(layer_names))
       } else{
         new_cols <- data.frame(matrix(data = 0, ncol= length(layer_names), nrow=nrow(spatial_grid), dimnames=list(NULL, layer_names)))
-        return(spatial_grid %>%
+        return(spatial_grid |>
                  dplyr::bind_cols(new_cols))
       }
     }
 
-    dat_grouped <- dat_cropped %>%
-      dplyr::group_by(.data[[feature_names]]) %>%
-      dplyr::summarise() %>%
-      dplyr::ungroup() %>%
-      {if(all(c("POLYGON", "MULTIPOLYGON") %in% (sf::st_geometry_type(.) %>% unique() %>% as.character()))) sf::st_cast(., to = "MULTIPOLYGON") else .}
+    dat_grouped <- dat_cropped |>
+      dplyr::group_by(.data[[feature_names]]) |>
+      dplyr::summarise() |>
+      dplyr::ungroup() |>
+      (\(x) if(all(c("POLYGON", "MULTIPOLYGON") %in% (sf::st_geometry_type(x) |>  unique() |>  as.character()))) sf::st_cast(x, to = "MULTIPOLYGON") else x)()
   }
   if(is_raster){
 
     nms <- dat_grouped[[1]]
 
- temp_rast  <- exactextractr::coverage_fraction(spatial_grid, dat_grouped) %>%
-      terra::rast() %>%
-      setNames(nms) %>%
-      terra::mask(spatial_grid) %>%
-      {if(!is.null(cutoff)) terra::classify(., matrix(c(-1, cutoff, 0, cutoff, 1.2, 1), ncol = 3, byrow = TRUE), include.lowest = FALSE, right = FALSE) else .}
+ temp_rast  <- exactextractr::coverage_fraction(spatial_grid, dat_grouped) |>
+      terra::rast() |>
+      setNames(nms) |>
+      terra::mask(spatial_grid) |>
+      (\(x) if(!is.null(cutoff)) terra::classify(x, matrix(c(-1, cutoff, 0, cutoff, 1.2, 1), ncol = 3, byrow = TRUE), include.lowest = FALSE, right = FALSE) else x)()
 
     #check if some features were cropped out; if so, need to add raster layers with zeroes in
     if(is.null(feature_names)) {
@@ -104,9 +112,9 @@ sf_to_grid <- function(spatial_grid, dat, matching_crs, name, feature_names, ant
       }  else{
         missing_feature_nms <- setdiff(unique(dat[[feature_names]]), nms)
 
-        missing_features_rast <- spatial_grid %>%
-          terra::subst(1,0) %>%
-          rep(length(missing_feature_nms)) %>%
+        missing_features_rast <- spatial_grid |>
+          terra::subst(1,0) |>
+          rep(length(missing_feature_nms)) |>
           setNames(missing_feature_nms)
 
         return(c(temp_rast, missing_features_rast))
@@ -117,43 +125,43 @@ sf_to_grid <- function(spatial_grid, dat, matching_crs, name, feature_names, ant
 
     if(grid_has_extra_cols) extra_cols <- sf::st_drop_geometry(spatial_grid)
 
-    spatial_grid_geom <- spatial_grid %>%
-      sf::st_geometry() %>%
+    spatial_grid_geom <- spatial_grid |>
+      sf::st_geometry() |>
       sf::st_sf()
 
-    spatial_grid_with_id <- spatial_grid_geom %>%
-      dplyr::mutate(cellID = 1:nrow(.))
+    spatial_grid_with_id <- dplyr::mutate(spatial_grid_geom, cellID = 1:nrow(spatial_grid_geom))
 
-    spatial_grid_with_area <- spatial_grid_with_id %>%
-      dplyr::mutate(area_cell = as.numeric(sf::st_area(.))) %>%
+    spatial_grid_with_area <- spatial_grid_with_id |>
+      (\(x) dplyr::mutate(x, area_cell = as.numeric(sf::st_area(x))))() |>
       sf::st_drop_geometry()
 
     layer_names <- if(is.null(feature_names)) name else dat_grouped[[1]]
 
-    dat_list <- if(is.null(feature_names)) list(dat_grouped) %>% setNames(layer_names) else split(dat_grouped, layer_names)
+    dat_list <- if(is.null(feature_names)) list(dat_grouped) |>  setNames(layer_names) else split(dat_grouped, layer_names)
 
     intersected_data_list <- list()
 
     for (layer in layer_names) {
-      temp_intersection <- sf::st_intersection(spatial_grid_with_id, dat_list[[layer]]) %>%
-        {if(all(sf::st_is_valid(.))) . else sf::st_make_valid(.)}
+      temp_intersection <- sf::st_intersection(spatial_grid_with_id, dat_list[[layer]]) |>
+        (\(x) if(all(sf::st_is_valid(x))) x else sf::st_make_valid(x))()
 
       if(nrow(temp_intersection)>0) {
-        intersected_data_list[[layer]] <- temp_intersection %>%
-          dplyr::mutate(area = as.numeric(sf::st_area(.))) %>%
-          sf::st_drop_geometry() %>%
-          dplyr::full_join(spatial_grid_with_area, ., by = c("cellID")) %>%
-          dplyr::mutate(perc_area = .data$area / .data$area_cell, .keep = "unused", .before = 1) %>%
-          dplyr::mutate(perc_area = dplyr::case_when(is.na(.data$perc_area) ~ 0,
-                                                     .default = as.numeric(.data$perc_area))) %>%
-          dplyr::left_join(spatial_grid_with_id, .,  by = "cellID") %>%
-          {if(is.null(cutoff)) dplyr::select(., .data$perc_area, {{layer}} := .data$perc_area) else {
-            dplyr::mutate(.,
-                          {{layer}} := dplyr::case_when(.data$perc_area >= cutoff  ~ 1,
-                                                    .default = 0)
-            ) %>%
+        intersected_data_list[[layer]] <- temp_intersection |>
+          dplyr::mutate(area = as.numeric(sf::st_area(temp_intersection))) |>
+          sf::st_drop_geometry() |>
+          dplyr::full_join(x = spatial_grid_with_area, y = _, by = c("cellID")) |>
+          (\(x) dplyr::mutate(x, perc_area = x$area / x$area_cell, .keep = "unused", .before = 1))() |>
+          (\(x) dplyr::mutate(x, perc_area = dplyr::case_when(is.na(x$perc_area) ~ 0,
+                                                              .default = as.numeric(x$perc_area))))() |>
+          dplyr::left_join(spatial_grid_with_id, y = _,  by = "cellID") |>
+          (\(x) if(is.null(cutoff)) {
+            dplyr::select(x, x$perc_area, {{layer}} := x$perc_area)}
+           else {
+            dplyr::mutate(x, {{layer}} := dplyr::case_when(x$perc_area >= cutoff  ~ 1,
+                                                           .default = 0)) |>
               dplyr::select({{layer}})
-          }}
+           })()
+        }
       } else{
         intersected_data_list[[layer]] <- spatial_grid_geom %>%
           dplyr::mutate({{layer}} := 0, .before = 1)
